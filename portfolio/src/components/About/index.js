b89daa0d3667c5e53824c38d74ba2563c6a70208
gavino41 <gavinosborne6910@gmail.com>
2023-06-01 00:04:51 -0400

    created and styled about me pages

---
+++ portfolio/src/components/About/index.js
@@ -0,0 +1,80 @@
+import { useEffect, useState } from 'react'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './index.scss'
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+     setTimeout(() => {
+      return setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+          I'm an extremely detail-oriented full-stack developer actively seeking
+           a role in a dynamic and innovative environment that allows me to 
+           engage with challenging projects utilizing cutting-edge and established technologies.
+          </p>
+          <p align="LEFT">
+          I exude quiet confidence, possess an innate curiosity, and am perpetually devoted to
+           enhancing my skills by tackling design problems, one at a time.
+          </p>
+          <p>
+            
+            If I were to encapsulate my essence in a single sentence, I would describe myself as an avid
+            aficionado of video games, a dedicated tech enthusiast, and profoundly passionate about the 
+            realm of software development.
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default About
